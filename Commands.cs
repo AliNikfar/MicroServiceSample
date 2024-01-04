770a0b55b9555d658758859fa363aed61e1274da
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-01-04 14:07:16 -0800

    update Docker-compose files and add portainer to the solution

---
+++ Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		//use for docker Update files
+		//1- add Container Orchestrator to the project  then select Linux option
+		//2- Update Docker-compose.yml and docker-compose.override.yml config files and add configurations
+		//3- run the following command to run updated Compose file to project
+		// docker-compose -f .\docker-compose.yml -f docker-compose.override.yml up -d
+
+		//to undo current done docker-compose to project use down intead of up in the command
+
+
+
+	}
+}
