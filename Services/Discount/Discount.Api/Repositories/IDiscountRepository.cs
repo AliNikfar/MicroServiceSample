0c9aa515a844330ddb9f022cbfe78928f9ac0923
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-01-05 09:22:35 -0800

    Add  NpgSQL and Dapper Add Dapper Repository And Implement CRUD for Coupone

---
+++ Services/Discount/Discount.Api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Api.Entities;
+
+namespace Discount.Api.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupone> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupone coupone);
+        Task<bool> UpdateDiscount(Coupone coupone);
+        Task<bool> DeleteeDiscount(string productName);
+    }
+}
