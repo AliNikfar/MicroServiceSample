4f5e246483a9bf98a3a236d3d706d09f783c3cb4
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-01-17 12:51:45 -0800

    Add Grpc Service Configuration

--- Services/Basket/Basket.Api/Program.cs
+++ Services/Basket/Basket.Api/Program.cs
@@ -1,19 +1,27 @@
+using Basket.Api.GrpsServices;
 using Basket.Api.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+IConfiguration configuration = builder.Configuration;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IBasketRepository,BasketRepository>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
+    options=> 
+    options.Address = new Uri(configuration["GrpcSetting:DiscountUrl"])
+    );
 builder.Services.AddStackExchangeRedisCache(option=>
 {
     option.Configuration = builder.Configuration.GetValue<string>("CachSettings:ConnectionString");
 });
-
+builder.Services.AddScoped<DiscountGrpcService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
