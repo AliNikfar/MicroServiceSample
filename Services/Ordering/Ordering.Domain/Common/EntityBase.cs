8bc2ed754395bf501a9d63dabec43b48b0b8deb0
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-01 14:46:18 -0800

    Refrence Order Projects  , Create Common And Entities Folder  and first Classes On Domain

---
+++ Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
