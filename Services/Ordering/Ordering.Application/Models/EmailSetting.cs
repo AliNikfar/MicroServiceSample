a848e03758af3819d005d1d8d24a5f8b591a8bb0
a.nikfarDPK <ali.nikfar2000@gmail.com>
2024-02-05 11:36:13 +0330

    Add Models , Contract and AutoMapper Sample in Architect

---
+++ Services/Ordering/Ordering.Application/Models/EmailSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    public class EmailSetting 
+    {
+        //public string From { get; set; }
+        //public string DisplayName { get; set; }
+        //public string UserName { get; set; }
+        //public string Password { get; set; }
+    }
+}
