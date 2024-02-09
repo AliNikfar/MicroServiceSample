db9bd6d844fb08a661055ac2b0e86359e27dc28c
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-09 11:39:49 -0800

    Add  NotFound , Validation   Exception ,ErrorHandle for  FuluentValidation

---
+++ Services/Ordering/Ordering.Application/Eexceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Eexceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name , Object key)
+            : base($"Entity \"{name}\" and key {key} was not found !")
+        {
+
+        }
+    }
+}
