14edd525974bade13720a6f6ca8d58a52fc72cfe
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-09 12:16:05 -0800

    Add First Step Of Pre And Post Processor With MediatR

---
+++ Services/Ordering/Ordering.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+
+        // this is a PreProcessor Befor run Handler
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+
+            }
+        }
+    }
+}
