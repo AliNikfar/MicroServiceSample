dea2dac7b41262b2c807174fee5b7643e27cd0db
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-09 06:22:24 -0800

    Finished UpdateCommand For Order

---
+++ Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidation : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidation()
+        {
+            RuleFor(a => a.UserName)
+                    .NotEmpty().WithMessage("{UserName} is Required")
+                    .NotNull()
+                    .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters");
+
+            RuleFor(a => a.EmailAddress)
+                    .NotEmpty().WithMessage("{EmailAddress} is required");
+
+            RuleFor(a => a.TotalPrice)
+                    .NotEmpty().WithMessage("{TotalPrice} is required")
+                    .GreaterThan(0).WithMessage("should bee greater than 0");
+        }
+    }
+}
