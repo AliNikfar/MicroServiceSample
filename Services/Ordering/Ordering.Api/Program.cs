fd8f23b946e96a098ecc7862ace7cef3ff706505
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-14 16:31:48 -0800

    Add DI Extension Configuration  For add a Services configuration File, For the Application That has No Program Or Startup File for Configure services

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -1,10 +1,12 @@
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationServices();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
