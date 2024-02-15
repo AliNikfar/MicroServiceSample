c7b0bd8ee1fbdc65015d05e73218b8f50154961c
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-02-15 14:48:15 -0800

    Implement "Generic Repository" Interface and it's Methods

--- Services/Ordering/Ordering.Application/Contracts/Persistance/IAsyncRepository.cs
+++ Services/Ordering/Ordering.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -11,7 +11,7 @@ namespace Ordering.Application.Contracts.Persistance
     public interface IAsyncRepository<T> where T : EntityBase
     {
         Task<IReadOnlyList<T>> GetAllAsync();
-        Task<IReadOnlyList<T>> GetByIdAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null ,
             Func<IQueryable<T>,IOrderedQueryable<T>> orderBy = null , 
             string includeString = null , bool disableTracking = true);
---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+    {
+        private readonly OrderContext _dbContext;
+        private  DbSet<T> _query;
+
+        public RepositoryBase(OrderContext dbContext)
+        {
+            _dbContext = dbContext;
+            _query = dbContext.Set<T>();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null, bool disableTracking = true)
+        {
+            var query = this._query.AsQueryable();
+            if (disableTracking)
+                query = query.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(includeString))
+                query = query.Include(includeString);
+            if (predicate != null) 
+                query = query.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+        {
+            var query = this._query.AsQueryable();
+            if (disableTracking)
+                query = query.AsNoTracking();
+            if (includes != null)
+                query = includes.Aggregate(query, (current , includes)=> current.Include(includes));
+            if (predicate != null)
+                query = query.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _query.Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _query.FindAsync(id);
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _query.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(T entity) 
+        {
+            _query.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
