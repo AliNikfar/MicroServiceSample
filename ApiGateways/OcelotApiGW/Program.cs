9c82bd30c7b22cd7d07cd5c62d3e0de1ae391070
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-03-31 16:46:56 -0700

    Finish implement of ocelot

--- ApiGateways/OcelotApiGW/Program.cs
+++ ApiGateways/OcelotApiGW/Program.cs
@@ -3,33 +3,34 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration configuration = builder.Configuration;
 
-builder.Services.AddOcelot();
-
-IConfiguration config = builder.Configuration;
-ConfigurationManager configuration = builder.Configuration;
-IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
-
-hostBuilder.ConfigureLogging((hostingContext,loggingbuilder) =>
+new WebHostBuilder()
+.UseKestrel()
+.UseContentRoot(Directory.GetCurrentDirectory())
+.ConfigureAppConfiguration((hostingContext, config) =>
 {
-    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-    loggingbuilder.AddConsole();
-    loggingbuilder.AddDebug();
-
-});
-hostBuilder.ConfigureAppConfiguration((hostingContext,config) =>
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddJsonFile("ocelot.json")
+        .AddEnvironmentVariables();
+})
+.ConfigureServices(s => {
+    s.AddOcelot();
+})
+.ConfigureLogging((hostingContext, logging) =>
 {
-    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true);
-});
+    logging.AddDebug();
+    //logging.AddConsole(); //Got Error when Activated
+    logging.AddConfiguration(configuration.GetSection("Logging"));
+})
+.UseIISIntegration()
+.Configure(app =>
+{
+    app.UseOcelot().Wait();
 
+})
+.Build()
+.Run();
 
-
-configuration.AddConfiguration(config.GetSection("Logging"));
-
-
-var app = builder.Build();
-await app.UseOcelot();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
