4e0445f70cca3e58a888014644a5965b15f5adb7
AliNikfarHome <ali.nikfar2000@gmail.com>
2024-04-01 15:26:22 -0700

    Add Extension of Generic Class to hadle every requests , without need to implement each one of models

---
+++ ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T> (this HttpResponseMessage response)
+        {
+            if(response.IsSuccessStatusCode)
+
+                throw new ApplicationException($"something went wrong when calling the api : {response.ReasonPhrase}");
+
+                var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString,new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+        }
+    }
+}
